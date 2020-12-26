dcd098c9337c5505bcf46094864682de79cab638
gulsahatalan <atalangulsah@gmail.com>
2020-12-26 01:17:51 +0100

    add linux-commands-packagers-editors.js

---
+++ shell-commands.sh
@@ -0,0 +1,23 @@
+Linux terminal komutlari
+
+1) pwd                                                             hangi dosyada/klasorde oldugunu gosterir
+2) ls -a                                                           gizli dosyalari da listeler
+3) mkdir gul                                                       gul  adinda klasor acar
+4) touch dosya1.txt                                                dosya1 dosyasini olusturur.
+5) cp dosya1.txt ./gul                                             dosya1.txt yi gul klasorune kopyalar
+6) cd gul/                                                         gul klasorune gecer
+7) rm dosya1.txt                                                   dosya1.txt siler
+8) echo “Birinci dosya temel shell komutlari” >> dosya2.txt        dosya2.txt adiyla dosya acip icine yazar
+9) cat dosya2.txt                                                  dosya2.txt in icerigini gosterir
+10) grep shell dosya2.txt                                          dosya2.txt dosyasinda shell kelimesini arar ve o satiri gosterir
+11) echo “Ikinci dosya disk islemleri” >> dosya3.txt               dosya3.txt adiyla dosya acip icine  yazar
+12) cat dosya2.txt dosya3.txt >> dosyaconcatanate.txt              iki dosyayi birlestirir   
+13) cd ..                                                          bir ust dizine cik
+14) rm -r gul/                                                     gul  klasorunu dolu olsa da siler
+15) sudo su                                                        parola yazdiktan sonra root yetkili hesaba gecer
+
+
+
+
+
+
